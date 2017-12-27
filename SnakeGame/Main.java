a4118b2332c43f8ab42a023fd3780cea84695ca2
unknown <amir77ni@gmail.com>
2017-12-27 18:08:35 +0330

    s

--- SnakeGame/Main.java
+++ SnakeGame/Main.java
@@ -1,5 +1,11 @@
+import java.awt.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Vector;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -7,24 +13,41 @@ public class Main {
 
 	public static void main(String[] args) {
 
-        String Name = JOptionPane.showInputDialog(null,"Please enter your name : ",
-				"Name",JOptionPane.QUESTION_MESSAGE);
-		Vector<String> levels = new Vector<>();
-		File curDir = new File(".");
-		File[] list = curDir.listFiles();
-		if(list!=null) {
-			for (File fil : list) {
+		try {
+			URL gameicon = new URL("https://raw.githubusercontent.com/amir-ni/SnakeGame/master/icon.png");
+			Image imgc = ImageIO.read(gameicon);
 
-				if (fil.getName().matches("level\\d.txt")) {
-					levels.add("Level "+fil.getName().substring(5,fil.getName().length()- 4));
-				}
-			}
+			String Name = JOptionPane.showInputDialog(null,"Please enter your name : ",
+					"Name",JOptionPane.QUESTION_MESSAGE);
+			Vector<String> levels = new Vector<>();
+			URL level = new URL("https://raw.githubusercontent.com/amir-ni/SnakeGame/master/levels/");
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(level.openStream()));
+
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+			//File curDir = new File(".");
+			//File[] list = curDir.listFiles();
+			//if(list!=null) {
+			//	for (File fil : list) {
+
+			//		if (fil.getName().matches("level\\d.txt")) {
+			//			levels.add("Level "+fil.getName().substring(5,fil.getName().length()- 4));
+			//		}
+			//	}
+			//}
+
+
+			ImageIcon img = new ImageIcon(imgc);
+			String level = (String) JOptionPane.showInputDialog(null,"Which level do you want to enter : " ,
+					"Level", JOptionPane.QUESTION_MESSAGE, img, levels.toArray(),levels.toArray()[0]);
+
+			new Snake(Name, Integer.parseInt(level.substring(6)));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
-		ImageIcon img = new ImageIcon("icon.png");
-		String level = (String) JOptionPane.showInputDialog(null,"Which level do you want to enter : " ,
-				"Level", JOptionPane.QUESTION_MESSAGE, img, levels.toArray(),levels.toArray()[0]);
-
-		new Snake(Name, Integer.parseInt(level.substring(6)));
 
 	}
 
